7aa5fbccb78e5a8bd4fcf50470d5382d7c907454
Alvaro Galvão Filho <alvaro.galvao.filho1580@gmail.com>
2025-05-12 19:52:04 -0300

    Criação da classe cliente

---
+++ models/cliente.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+
+@dataclass
+
+class Cliente:
+    id: int
+    nome: str
+    cpf: str
+    telefone: str
+    email: str
+    endereco: str
+    data_nascimento: datetime
+
+    def __str__(self):
+        return f"Cliente(id={self.id}, nome={self.nome}, cpf={self.cpf}, telefone={self.telefone}, email={self.email}, endereco={self.endereco}, data_nascimento={self.data_nascimento})"
